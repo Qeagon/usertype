b58515305ab9b89feeaf087328aa946b84c6475f
Jedidja Mansvelder <jedidja@MacBookPro.home>
2026-03-27 16:08:26 +0100

    made new folders to make my work easy to read made a folder for the guards,user  service and usertypes also completed each function

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import{ UserService } from './services/user.service';
+import { Admin, Moderator, RegularUser, SuperUser } from './models/user.types';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
